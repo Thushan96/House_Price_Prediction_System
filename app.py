9510f6b31634f82f887ef66655cc889642e120eb
Thushan96 <thushanmadhulakshitha@gmail.com>
2023-06-23 22:23:41 +0530

    updated content

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template
 import pickle
 
-app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
+features_name = ['YearBuilt', 'Area', 'NumBedRooms', 'AreaBedroom', 'BedroomCond', 'NumKitch', 'AreaKitch',
+                 'KitchCond', 'Garage', 'GarageArea', 'Electricity', 'AirConditioning', 'NumHearth',
+                 'HouseCondition', 'Pool', 'Garden']
 
-@app.route("/")
-def home():
-    return render_template('index.html')
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    input_data = [float(x) for x in request.form.values()]
-    variables_data = [np.array(input_data)]
-
-    features_name = ['YearBuilt', 'Area', 'NumBedRooms', 'AreaBedroom', 'BedroomCond', 'NumKitch', 'AreaKitch',
-                     'KitchCond', 'Garage', 'GarageArea', 'Electricity', 'AirConditioning', 'NumHearth',
-                     'HouseCondition', 'Pool', 'Garden']
-
-    df = pd.DataFrame(variables_data, columns=features_name)
+def predict(input_data):
+    df = pd.DataFrame([input_data], columns=features_name)
     house_price = int(model.predict(df))
-    return render_template('index.html', prediction_text='The Estimated Price for the House is Rs.{}.00'.format(house_price))
+    return house_price
+
+
+def main():
+    st.title('House Price Prediction')
+    st.write('Enter the following details to predict the house price:')
+
+    input_data = []
+    for feature in features_name:
+        if feature.endswith('Cond'):
+            input_val = st.selectbox(f'{feature}:', ['Excellent', 'Very Good', 'Good', 'Average', 'Typical', 'Bad', 'Very Bad'])
+        elif feature in ['Garage', 'Electricity', 'Pool', 'Garden']:
+            input_val = st.selectbox(f'{feature}:', ['Yes', 'No'])
+        else:
+            input_val = st.text_input(f'{feature}:')
+
+        input_data.append(input_val)
+
+    if st.button('Predict'):
+        house_price = predict(input_data)
+        st.success(f'The Estimated Price for the House is Rs.{house_price}.00')
 
 
 if __name__ == "__main__":
-    app.run()
+    main()
